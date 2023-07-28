d459d4cf3277beeab041fea604fa1c513c13f67d
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2023-07-28 17:32:30 +0530

    feat: added model for customerlog

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class CustomerLog(models.Model):
+    customer_id = models.CharField(max_length=255)
+    request_time = models.DateTimeField(auto_now_add=True)
+    error_msg = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"Customer --- {self.customer_id} (Request time -- {self.request_time})"
